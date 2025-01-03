72a9257f1746c337d7f3e8e02ce97ba89c3a200e
BilalSubhani <mailatbilalsubhani@gmail.com>
2025-01-03 13:42:01 +0500

    Added APi for uploading and fetching video media

--- src/media/media.controller.ts
+++ src/media/media.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, UseInterceptors, UploadedFile, Get, Param, Res, NotFoundException } from '@nestjs/common';
+import { Controller, Post, UseInterceptors, UploadedFile, Get, Param, Res, NotFoundException, BadRequestException } from '@nestjs/common';
 import { Response } from 'express';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { MediaService } from './media.service';
@@ -23,6 +23,31 @@ export class MediaController {
         return res.send(file.data);
         } catch (error) {
         throw new NotFoundException('File not found');
+        }
+    }
+
+    @Post('upload-video')
+    @UseInterceptors(FileInterceptor('file', {
+        limits: { fileSize: 5 * 1024 * 1024 }, 
+    }))
+    async uploadVideo(@UploadedFile() file: Express.Multer.File) {
+        if (!file) {
+        throw new BadRequestException('Video file is required');
+        }
+
+        return this.mediaService.storeFile(file);
+    }
+
+    @Get('video/:id')
+    async getVideo(@Param('id') id: string, @Res() res: Response) {
+        try {
+        const file = await this.mediaService.getFile(id);
+
+        res.set('Content-Type', file.contentType);
+
+        return res.send(file.data);
+        } catch (error) {
+        throw new NotFoundException('Video not found');
         }
     }
 }
